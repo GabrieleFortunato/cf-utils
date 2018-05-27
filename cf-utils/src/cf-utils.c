d90bdbfbc82ecd00b244590ca4a3a53b80d92b35
GabrieleFortunato <gabrielefortunato78@libero.it>
2018-05-27 05:01:16 +0200

    Implementate le post condizioni

--- cf-utils/src/cf-utils.c
+++ cf-utils/src/cf-utils.c
@@ -6,71 +6,78 @@
  */
 
 #include "cf-utils.h"
+#include <stdio.h>
 
 static bool isvowel(char a){
-	return
-			a==A||a==tolower(A)||a==E||a==tolower(E)||a==I||a==tolower(I)||
-			a==O||a==tolower(O)||a==U||a==tolower(U);
+	return a==A||a==tolower(A)||a==E||a==tolower(E)||a==I||
+			a==tolower(I)||a==O||a==tolower(O)||a==U||a==tolower(U);
 }
 
-static bool iscons(char a){
-	return isalpha(a)&&!isvowel(a);
+static void getvowel(const char* a, char* b, int i, int* k){
+	if (isvowel(a[i]))
+		b[(*k)++]=a[i];
 }
 
-static bool all_vow(char* text){
-	bool flag=true;
-	for (int i=ZERO;i<strlen(text)&&flag;i++)
-		flag= isvowel(text[i]);
-	return flag;
-}
-
-static bool all_cons(char* text){
-	bool flag=true;
-	for (int i=ZERO;i<strlen(text)&&flag;i++)
-		flag= iscons(text[i]);
-	return flag;
-}
-
-static bool char_in_text(const char* text, char a){
-	bool flag = false;
-	for (int i=ZERO;i<strlen(text)&&!flag;i++)
-		flag = (a==text[i]);
-	return flag;
-}
-
-static bool text_in_text(const char* text, char* a){
-	bool flag = true;
-	for (int i=ZERO;i<strlen(text)&&!flag;i++)
-		flag = char_in_text(text,a[i]);
-	return flag;
-}
-
-static void get_consonant(const char* text, char* cons, int* k, int i){
-	if (iscons(text[i]))
-		cons[(*k)++]=text[i];
-}
-
-static void get_consonants(const char* text, char* cons){
+static void getvowels(const char* text, char* vow){
 	int k=ZERO;
 	for (int i=ZERO;i<strlen(text);i++)
-		get_consonant(text,cons,&k,i);
-	cons[k]=EOS;
-}
-
-static void get_vowel(const char* text, char* vow, int* k, int i){
-	if (isvowel(text[i]))
-		vow[(*k)++]=text[i];
-}
-static void get_vowels(const char* text, char* vow){
-	int k=ZERO;
-	for (int i=ZERO;i<strlen(text);i++)
-		get_vowel(text,vow,&k,i);
+		getvowel(text,vow,i,&k);
 	vow[k]=EOS;
 }
 
+static bool isconsonant(char a){
+	return isalpha(a)&&!isvowel(a);
+}
+
+static void getconsonant(const char* a, char* b, int i, int* k){
+	if (isconsonant(a[i]))
+		b[(*k)++]=a[i];
+}
+
+static void getconsonants(const char* text, char* cons){
+	int k=ZERO;
+	for (int i=ZERO;i<strlen(text);i++)
+		getconsonant(text,cons,i,&k);
+	cons[k]=EOS;
+}
+
+static bool allconsonants(char* string){
+	bool flag = true;
+	for (int i=ZERO;i<strlen(string)&&flag;i++)
+		flag = isconsonant(string[i]);
+	return flag;
+}
+
+static bool allvowels(char* string){
+	bool flag = true;
+	for (int i=ZERO;i<strlen(string)&&flag;i++)
+		flag = isvowel(string[i]);
+	return flag;
+}
+
+static bool charintext(const char* text, char string){
+	bool flag = false;
+	for (int i=ZERO;i<strlen(text)&&!flag;i++)
+		flag = (text[i]==string)?true:false;
+	return flag;
+}
+
+static bool textintext(const char* text, char* string){
+	bool flag;
+	for (int i=ZERO;i<strlen(string);i++)
+		flag = charintext(text,string[i]);
+	return flag;
+}
+
 void cons_vow(const char* text, char* cons, char* vow){
-	assert(text!=NULL&&cons!=NULL&&vow!=NULL);
-	get_vowels(text,vow);
-	get_consonants(text,cons);
-	assert(all_vow(vow)&&all_cons(cons)&&text_in_text(text,vow)&&text_in_text(text,cons));
+	assert(cons!=NULL);
+	assert(text!=NULL);
+	assert(vow!=NULL);
+	getvowels(text,vow);
+	getconsonants(text,cons);
+	assert(strlen(text)>=(strlen(cons)+strlen(vow)));
+	assert(allconsonants(cons));
+	assert(allvowels(vow));
+	assert(textintext(text,vow));
+	assert(textintext(text,cons));
 }
