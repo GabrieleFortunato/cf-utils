f943c8003564742616b5ba8e0f1146dbf118cea7
GabrieleFortunato <gabrielefortunato78@libero.it>
2018-08-30 16:31:57 +0200

    Commit del 30 agosto 2018 ore 16,31

--- cf-utils/html/search/all_63.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['cf_2dutils_2ec',['cf-utils.c',['../cf-utils_8c.html',1,'']]],
-  ['cf_2dutils_2eh',['cf-utils.h',['../cf-utils_8h.html',1,'']]],
-  ['cons_5fvow',['cons_vow',['../cf-utils_8c.html#a4721e1e7e0b6b0fece1aafcc9d4e0077',1,'cons_vow(const char *text, char *cons, char *vow):&#160;cf-utils.c'],['../cf-utils_8h.html#a4721e1e7e0b6b0fece1aafcc9d4e0077',1,'cons_vow(const char *text, char *cons, char *vow):&#160;cf-utils.c']]]
-];
--- cf-utils/src/cf-utils.c
+++ cf-utils/src/cf-utils.c
@@ -7,50 +7,27 @@
 
 #include "cf-utils.h"
 
-static const int ZERO = 0;
 static const char EOS = '\0';
+static const int ZERO = 0;
+static const char u = 'u';
 static const char U = 'U';
+static const char o = 'o';
 static const char O = 'O';
+static const char i = 'i';
 static const char I = 'I';
+static const char e = 'e';
 static const char E = 'E';
+static const char a = 'a';
 static const char A = 'A';
 
-static bool is_vowel(char a){
-	return a == A || a == tolower(A) || a == E || a == tolower(E) || a == I
-			|| a == tolower(I) || a == O || a == tolower(O) ||
-			a == U || a == tolower(U);
+static bool is_vowel(char letter){
+	return letter == A || letter == a || letter == E || letter == e	|| letter == I ||
+			letter == i || letter == O || letter == o
+			|| letter == U || letter == u;
 }
 
 static bool is_consonant(char a){
 	return isalpha(a)&&!is_vowel(a);
-}
-
-static bool all_vowels(char* text){
-	bool flag = true;
-	for (int i = ZERO; i < strlen(text) && flag; i++)
-		flag = is_vowel(text[i]);
-	return flag;
-}
-
-static bool all_consonants(char* text){
-	bool flag = true;
-	for (int i = ZERO; i < strlen(text) && flag; i++)
-		flag = is_consonant(text[i]);
-	return flag;
-}
-
-static bool char_in_text(const char* a, char b){
-	bool flag = false;
-	for (int i = ZERO; i < strlen(a) && !flag; i++)
-		flag = (a[i]==b);
-	return flag;
-}
-
-static bool text_in_text(const char* a, char* b){
-	bool flag = true;
-	for (int i = ZERO; i < strlen(b) && flag; i++)
-		flag = char_in_text(a,b[i]);
-	return flag;
 }
 
 static int get_vowel(const char* text, int i, int j, char* vow) {
@@ -79,8 +56,36 @@ static void get_consonants(const char* text, char* cons) {
 	cons[j] = EOS;
 }
 
-static bool are_valid_length(const char* text, char* cons, char* vow) {
-	return strlen(text) >= strlen(cons) + strlen(vow);
+static bool lengths(const char* text, char* cons, char* vow){
+	return strlen(text)>=(strlen(cons)+strlen(vow));
+}
+
+static bool all_consonants(char* cons){
+	bool flag = true;
+	for (int i = ZERO; i < strlen(cons) && flag;)
+		flag = is_consonant(cons[i++]);
+	return flag;
+}
+
+static bool all_vowels(char* cons){
+	bool flag = true;
+	for (int i = ZERO; i < strlen(cons) && flag;)
+		flag = is_vowel(cons[i++]);
+	return flag;
+}
+
+static bool char_in_text(const char* text, char a){
+	bool flag = false;
+	for (int i = ZERO; i < strlen(text) && !flag;)
+		flag = (a == text[i++]);
+	return flag;
+}
+
+static bool text_in_text(const char* text1, char* text2){
+	bool flag = true;
+	for (int i = ZERO; i < strlen(text2) && flag;)
+		flag = char_in_text(text1,text2[i++]);
+	return flag;
 }
 
 void cons_vow(const char* text, char* cons, char* vow){
@@ -89,9 +94,9 @@ void cons_vow(const char* text, char* cons, char* vow){
 	assert(vow!=NULL);
 	get_vowels(text, vow);
 	get_consonants(text, cons);
-	assert(are_valid_length(text,cons,vow));
-	assert(all_vowels(vow));
+	assert(lengths(text,cons,vow));
 	assert(all_consonants(cons));
+	assert(all_vowels(vow));
 	assert(text_in_text(text,cons));
 	assert(text_in_text(text,vow));
 }
